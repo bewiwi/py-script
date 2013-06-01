5be5413624cd867af9764b0909307ccf88ad31ad
Bewiwi <bewiwi@bibabox.fr>
2013-06-01 18:37:46 +0200

    Ajout d'un script de control des GPIO pour raspberry pi

---
+++ script/rb.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+import RPi.GPIO as GPIO
+from time import sleep
+import sys,getopt
+gpio=25
+statu=-1
+try:
+        opts,args = getopt.getopt(sys.argv[1:],'hs:i:')
+except getopt.GetoptError:
+        print "error ARg"
+        sys.exit(1)
+for opt,arg in opts:
+        if opt == '-s':
+                statu=arg
+        elif opt == '-i':
+                gpio=arg
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(gpio, GPIO.OUT)
+if statu == '1':
+        GPIO.output(gpio,True)
+elif statu == '0':
+        GPIO.output(gpio,False)
+else:
+        print GPIO.input(gpio)
